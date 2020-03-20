968401ce30e141d321259d1d0b4bf05486389466
Ruben Espinosa <ruben.espinosa@eventbrite.com>
2020-03-20 18:51:21 +0100

    Add logs

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ try {
   console.log(`Checking version: ${version}!`);
   
   var isValid = false;
-  var re = new RegExp('[0-9]+\.[0-9]+\.[0-9]+(-(0|[1-9]\d*|(beta|alpha).*))?$');
+  console.log(`isValid false`);
+  var re = new RegExp("[0-9]+\.[0-9]+\.[0-9]+(-(0|[1-9]\d*|(beta|alpha).*))?$");
+  console.log(`RegExp`);
   if (re.exec(version) != null) {
     isValid = true;
+    console.log(`isValid true`);
   }
   core.setOutput("is-valid", isValid);
+  console.log(`setOutput`);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
